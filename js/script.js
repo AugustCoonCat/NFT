ccae0a6f35de3f8ef0d1d7000db599e89bf99e0c
Misha <misha.yakovlev1755@gmail.com>
2025-03-19 17:33:16 +0300

    Доработки

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const abstraction1=document.getElementById("abstraction1")
+const chooseButton=document.getElementById("choose_button")
+
+chooseButton.addEventListener("click",()=>{
+    abstraction1.classList.add("create__right-abstraction1_active") 
+})
