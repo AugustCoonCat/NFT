06244a7dcba4f3f7cc3d30e2c7d0b94c84782055
Misha <misha.yakovlev1755@gmail.com>
2025-04-02 16:41:40 +0300

    Доделки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const abstraction1 = document.getElementById("abstraction1");
-const chooseButton = document.getElementById("choose_button");
+const chooseButton = document.getElementById("features__navigation");
 
 chooseButton.addEventListener("click", () => {
   abstraction1.classList.add("features__right-abstraction1_active");
